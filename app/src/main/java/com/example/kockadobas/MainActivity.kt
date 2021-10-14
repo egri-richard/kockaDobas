c4d804cef13911afcaf5bc555eb1cdf8864fa296
Erinad-2002 <Erinad.business@gmail.com>
2021-10-14 23:11:27 +0200

    dice throw made into its separate function and game half complete

--- app/src/main/java/com/example/kockadobas/MainActivity.kt
+++ app/src/main/java/com/example/kockadobas/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import java.util.*
 
@@ -17,11 +18,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var kocka2btn: Button
     private lateinit var throwBtn: Button
     private lateinit var resetBtn: Button
+    private lateinit var gameBtn: Button
     private lateinit var rnd: Random
     private lateinit var resetAlert: AlertDialog.Builder
+    private lateinit var newGameAlert: AlertDialog.Builder
+    private lateinit var gameComplete: AlertDialog.Builder
     private var numOfKocka = 2
     private var kocka1val = 0
     private var kocka2val = 0
+    private var enemyVal = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,50 +46,41 @@ class MainActivity : AppCompatActivity() {
         }
 
         throwBtn.setOnClickListener {
-            if (numOfKocka == 2) {
-                kocka1val = rnd.nextInt(6)+1
-                kocka2val = rnd.nextInt(6)+1
-
-                when(kocka1val) {
-                    1 -> kocka1img.setImageResource(R.drawable.kocka1)
-                    2 -> kocka1img.setImageResource(R.drawable.kocka2)
-                    3 -> kocka1img.setImageResource(R.drawable.kocka3)
-                    4 -> kocka1img.setImageResource(R.drawable.kocka4)
-                    5 -> kocka1img.setImageResource(R.drawable.kocka5)
-                    6 -> kocka1img.setImageResource(R.drawable.kocka6)
-                }
-
-                when(kocka2val) {
-                    1 -> kocka2img.setImageResource(R.drawable.kocka1)
-                    2 -> kocka2img.setImageResource(R.drawable.kocka2)
-                    3 -> kocka2img.setImageResource(R.drawable.kocka3)
-                    4 -> kocka2img.setImageResource(R.drawable.kocka4)
-                    5 -> kocka2img.setImageResource(R.drawable.kocka5)
-                    6 -> kocka2img.setImageResource(R.drawable.kocka6)
-                }
-
-                val sum = kocka1val + kocka2val
-                scoreDisplay.append("($kocka1val + $kocka2val) $sum \n")
-            }
-            else {
-                kocka1val = rnd.nextInt(6)+1
-
-                when(kocka1val) {
-                    1 -> kocka1img.setImageResource(R.drawable.kocka1)
-                    2 -> kocka1img.setImageResource(R.drawable.kocka2)
-                    3 -> kocka1img.setImageResource(R.drawable.kocka3)
-                    4 -> kocka1img.setImageResource(R.drawable.kocka4)
-                    5 -> kocka1img.setImageResource(R.drawable.kocka5)
-                    6 -> kocka1img.setImageResource(R.drawable.kocka6)
-                }
-
-                scoreDisplay.append("$kocka1val \n")
-            }
+            diceThrow()
         }
 
         resetBtn.setOnClickListener {
             val dialog = resetAlert.create()
             dialog.show()
+        }
+
+        gameBtn.setOnClickListener {
+            val dialog = newGameAlert.create()
+            dialog.show()
+            var click = false
+            var click2 = false
+            throwBtn.setOnClickListener {
+                diceThrow()
+                click = true
+            }
+
+            if (click) {
+                Toast.makeText(this, "Valassz kockat amit ujra szeretnel dobni", Toast.LENGTH_LONG).show()
+                // TODO: 2021. 10. 14. change imageView to ImageButton and make onclick events 
+                if (click2) {
+                    val playerVal = kocka1val + kocka2val
+                    if (enemyVal > playerVal) {
+                        val dialog = gameComplete.create()
+                            dialog.setTitle("Vesztettel")
+                            dialog.show()
+                    }
+                    else {
+                        val dialog = gameComplete.create()
+                        dialog.setTitle("Nyertel")
+                        dialog.show()
+                    }
+                }
+            }
         }
     }
 
@@ -96,6 +92,7 @@ class MainActivity : AppCompatActivity() {
         kocka2btn = findViewById(R.id.kocka2btn)
         throwBtn = findViewById(R.id.throwBtn)
         resetBtn = findViewById(R.id.resetBtn)
+        gameBtn = findViewById(R.id.gameBtn)
         rnd = Random()
         resetAlert = AlertDialog.Builder(this@MainActivity)
         resetAlert
@@ -103,6 +100,57 @@ class MainActivity : AppCompatActivity() {
             .setMessage("Biztosan torli az eddigi dobasokat?")
             .setPositiveButton("igen") {_, _ -> reset()}
             .setNegativeButton("nem") {_, _ ->}
+        newGameAlert = AlertDialog.Builder(this@MainActivity)
+            .setTitle("Uj Jatek")
+            .setMessage("Szeretne uj jatekot kezdeni?/nJatszani csak 2 kockaval lehet")
+            .setPositiveButton("igen") {_, _ -> newGame()}
+            .setNegativeButton("nem") {_, _ ->}
+        gameComplete = AlertDialog.Builder(this)
+            .setMessage("Szeretnel ujra jatszani?")
+            .setPositiveButton("igen") {_, _ -> newGame()}
+            .setNegativeButton("nem") {_, _ ->}
+    }
+
+    private fun diceThrow() {
+        if (numOfKocka == 2) {
+            kocka1val = rnd.nextInt(6)+1
+            kocka2val = rnd.nextInt(6)+1
+
+            when(kocka1val) {
+                1 -> kocka1img.setImageResource(R.drawable.kocka1)
+                2 -> kocka1img.setImageResource(R.drawable.kocka2)
+                3 -> kocka1img.setImageResource(R.drawable.kocka3)
+                4 -> kocka1img.setImageResource(R.drawable.kocka4)
+                5 -> kocka1img.setImageResource(R.drawable.kocka5)
+                6 -> kocka1img.setImageResource(R.drawable.kocka6)
+            }
+
+            when(kocka2val) {
+                1 -> kocka2img.setImageResource(R.drawable.kocka1)
+                2 -> kocka2img.setImageResource(R.drawable.kocka2)
+                3 -> kocka2img.setImageResource(R.drawable.kocka3)
+                4 -> kocka2img.setImageResource(R.drawable.kocka4)
+                5 -> kocka2img.setImageResource(R.drawable.kocka5)
+                6 -> kocka2img.setImageResource(R.drawable.kocka6)
+            }
+
+            val sum = kocka1val + kocka2val
+            scoreDisplay.append("($kocka1val + $kocka2val) $sum \n")
+        }
+        else {
+            kocka1val = rnd.nextInt(6)+1
+
+            when(kocka1val) {
+                1 -> kocka1img.setImageResource(R.drawable.kocka1)
+                2 -> kocka1img.setImageResource(R.drawable.kocka2)
+                3 -> kocka1img.setImageResource(R.drawable.kocka3)
+                4 -> kocka1img.setImageResource(R.drawable.kocka4)
+                5 -> kocka1img.setImageResource(R.drawable.kocka5)
+                6 -> kocka1img.setImageResource(R.drawable.kocka6)
+            }
+
+            scoreDisplay.append("$kocka1val \n")
+        }
     }
 
     private fun reset() {
@@ -110,5 +158,11 @@ class MainActivity : AppCompatActivity() {
         kocka2img.visibility = View.VISIBLE
         kocka1img.setImageResource(R.drawable.kocka1)
         kocka2img.setImageResource(R.drawable.kocka1)
+    }
+
+    private fun newGame() {
+        scoreDisplay.text = ""
+        Toast.makeText(this@MainActivity, "Dobj", Toast.LENGTH_SHORT).show()
+        enemyVal = (rnd.nextInt(6)+1) + (rnd.nextInt(6)+1)
     }
 }
